a3f043e3338c5c2a9fe2b4cdae145a70acde85e5
jd richards <jdrichardstech@gmail.com>
2024-02-05 13:19:35 -0500

    update App instr

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from './components/Navbar'
 import SeparatorOne from './components/SeparatorOne'
 
-//the NavBar which is singly nested in the App.js can consume the context. Meanwhile so can the deeply next Form.js component
+//the NavBar which is singly nested in the App.jsx can consume the context. Meanwhile so can the deeply nested Form.jsx component which is consumed in the SeparatorTwo Component.
 const App = () => {
   return (
     <>
