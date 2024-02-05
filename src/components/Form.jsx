a6d58082dca98b86b6cf80da5c4a3f0e0b6e92de
jd richards <jdrichardstech@gmail.com>
2024-02-05 12:19:51 -0500

    update code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,46 +1,45 @@
-import { useState } from "react";
-//This is how I import and consume the context if I want to use the custom hook from LanguageContext
-import { useLanguage } from "../context/LanguageContext";
+import { useState } from 'react'
 
-//the NavBar which is singly nested in the App.js can consume the context. Meanwhile so can this deeply next Form.js component
 export default function Form() {
-  // destructure and consume only the data, state or functions that I need from the component
-  const { language, setLanguage, english, french } = useLanguage();
-
-  const [name, setName] = useState({ firstName: "", lastName: "" });
+  const [name, setName] = useState({ firstName: '', lastName: '' })
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const { greeting, instruction, select, firstName, lastName, button } =
-    language === "english" ? english : french;
+    e.preventDefault()
+    // no need to make this work. out of scope of project
+  }
 
   return (
     <>
-      <h1>{greeting} 😃!</h1>
+      <h1>Hello 😃!</h1>
       <h2>
-        {select} <span onClick={() => setLanguage("french")}>FR</span> |
-        <span onClick={() => setLanguage("english")}>EN</span>
+        Choose Your Language <span>FR</span> |<span>EN</span>
       </h2>
-      <p>{instruction}</p>
+      <p>Please Fill Out This Form</p>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder={name.firstName}
-          value={name.firstName}
-          id={name.firstName}
-          onChange={(e) => setName({ ...name, [e.target.id]: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder={name.lastName}
-          value={name.lastName}
-          id={name.lastName}
-          onChange={(e) => setName({ ...name, [e.target.id]: e.target.value })}
-        />
-        <button type="submit">{button}</button>
+        <label htmlFor="firstName">
+          <input
+            type="text"
+            placeholder="First Name"
+            value={name.firstName}
+            id="firstName"
+            onChange={(e) =>
+              setName({ ...name, [e.target.id]: e.target.value })
+            }
+          />
+        </label>
+        <label htmlFor="lastName">
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={name.lastName}
+            id="lastName"
+            onChange={(e) =>
+              setName({ ...name, [e.target.id]: e.target.value })
+            }
+          />
+        </label>
+        <button type="submit">Submit</button>
       </form>
     </>
-  );
+  )
 }
